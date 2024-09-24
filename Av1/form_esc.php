9483419319c787326e905228fcb8186b516398ef
Augusto_Mac <135230150+AugustoMaced@users.noreply.github.com>
2024-09-24 11:21:10 -0300

    Um Crud utilizando o PhP
    O Sr. Water Falls precisa de um sistema de jogo corporativo, para treinar seus gestores em situações difíceis. O jogo deverá gerenciar situações de perguntas e respostas (decisões) encadeadas.
    O game é composto por vários desafios e cada desafio tem um objetivo específico, como por exemplo, gerenciar o andamento de um projeto, resolver um problema administrativo, contratar um novo funcionário, conceder um empréstimo e outros.
    Neste primeiro momento será desenvolvido somente o cadastro Usuários, Perguntas e Respostas.
    Criar as funcionalidades de Criar Perguntas e respostas de multipla escolha, Criar Perguntas e respostas de texto,  alterar Perguntas e suas respostas de multipla escolha, listar todas Perguntas, listar uma Pergunta e excluir Pergunta e respostas.
    Inicialmente usaremos arquivos texto(txt) para salvar os usuários.
    As funcionalidades de Perguntas e respostas devem estar disponíveis por tela.
    O código deverá ser em PHP.
    Então deverá ser criado:
    1. Criar Perguntas e respostas de multipla escolha.
    2.Criar Perguntas e respostas de texto.
    3. Alterar Perguntas e suas respostas de multipla escolha
    4. Alterar Perguntas com respostas de texto
    5. Listar Perguntas e repostas.
    6. Listar uma Pergunta.
    7. Excluir Pergunta e respostas

---
+++ Av1/form_esc.php
@@ -0,0 +1,113 @@
+<?php
+  $pergunta = "";
+  $r1 = "";
+  
+  if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $pergunta = $_POST["pergunta"];
+    $r1 = $_POST["r1"];
+   
+    if(!(file_exists("escritas.txt"))){
+      $arqDisc=fopen(filename: "escritas.txt", mode:"w") or die("Erro ao criar arquivo!");
+      $linha = "Pergunta;Resposta\n";
+      fwrite($arqDisc,$linha);
+      fclose($arqDisc);
+    } 
+      $arqDisc=fopen(filename: "escritas.txt", mode:"a") or die("Erro ao criar arquivo"); 
+      $linha = $pergunta . ";" . $r1 . "\n";
+  fwrite($arqDisc,$linha);
+  fclose($arqDisc);
+    
+  }
+?>
+
+<html>
+  <head>
+    <style>
+      body{
+        margin: 0;
+        padding: 0;
+        background: #2d3436;
+      }
+      .middle{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+      }
+      .btn{
+        position: relative;
+        display: block;
+        color: white;
+        font-size: 14px;
+        font-family: "montserrat";
+        text-decoration: none;
+        margin: 30px 0;
+        border: 2px solid lightskyblue;
+        padding: 14px 60px;
+        text-transform: uppercase;
+        overflow: hidden;
+        transition: 1s all ease;
+      }
+      .btn::before{
+        background: lightskyblue;
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        z-index: -1;
+        transition: all 0.6s ease;
+      }
+      
+      .btn1::before{
+        width: 0%;
+        height: 100%;
+      }
+      
+      .btn1:hover::before{
+        width: 100%;
+      }
+      
+      
+      .btn2::before{
+        width: 100%;
+        height: 0%;
+      }
+      .btn2:hover::before{
+        height: 100%;
+      }
+      
+      .btn3::before{
+        width: 100%;
+        height: 0%;
+        transform: translate(-50%,-50%) rotate(45deg);
+      }
+      .btn3:hover::before{
+        height: 380%;
+      }
+      
+      .btn4::before{
+        width: 100%;
+        height: 0%;
+        transform: translate(-50%,-50%) rotate(-45deg);
+      }
+      .btn4:hover::before{
+        height: 380%;
+      }
+    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>JOGOS COORPORATIVOS </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Syncopate:wght@700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <br>
+    <center><h2 style="color:lightskyblue;">Cadastro de pergunta e resposta realizado com sucesso!</h2></center>
+    <a href="index.html" style="font-family: 'Montserrat', sans-serif;color: white;">RETORNAR</a>
+  </body>
+</html>
